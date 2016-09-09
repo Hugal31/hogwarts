29fcf373fc323d8d22a09753c8b6d3ecadaffa57
laloge_h <hugo.laloge@epitech.eu>
2016-09-09 18:39:33 +0200

    feat: Refresh score every minute

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,15 @@
 var hogwartsApp = angular.module('hogwartsApp', []);
 
-hogwartsApp.controller('HourglassController', function HourglassController($scope, $http) {
-    $http.get('/api/v0/houses').then(function(response) {
-        $scope.houses = response.data.houses;
-    });
+hogwartsApp.controller('HourglassController', function HourglassController($scope, $http, $interval) {
+
+    $scope.update = function () {
+        $http.get('/api/v0/houses').then(function(response) {
+            $scope.houses = response.data.houses;
+        });
+    };
+
+    $scope.update();
+
+    // Refresh every minute
+    $interval($scope.update, 60000);
 });
