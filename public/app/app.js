04fe51f1a0abf64456c2766be124f32a09264a22
laloge_h <hugo.laloge@epitech.eu>
2016-09-10 01:22:36 +0200

    feat: Beautiful design for front

--- public/app/app.js
+++ public/app/app.js
@@ -2,12 +2,16 @@ var hogwartsApp = angular.module('hogwartsApp', []);
 
 hogwartsApp.controller('HourglassController', function HourglassController($scope, $http, $interval) {
 
+    $scope.interval = 30000;
+    $scope.maxScore = 500;
     $scope.houses = [];
 
     $scope.update = function () {
         $http.get('/api/v0/houses').then(function(response) {
             response.data.forEach(function (house) {
                 $scope.houses[house.name] = house;
+                var maxHeight = parseInt($('.hourglass[data-house="' + house.name + '"] .hourglass-body .middle').css('height'));
+                $('.hourglass[data-house="' + house.name + '"] .hourglass-body .middle .sand').css('height', maxHeight * house.score / $scope.maxScore);
             });
         });
     };
@@ -15,5 +19,5 @@ hogwartsApp.controller('HourglassController', function HourglassController($scop
     $scope.update();
 
     // Refresh every minute
-    $interval($scope.update, 60000);
+    $interval($scope.update, $scope.interval);
 });
--- resources/views/index.php
+++ resources/views/index.php
@@ -3,30 +3,61 @@
   <head>
     <meta charset="UTF-8">
     <title>Epi Hogwarts</title>
-    <link rel="stylesheet" href="/app/app.css">
+    <link rel="stylesheet" href="/app/app.css"><script
+        src="https://code.jquery.com/jquery-3.1.0.min.js"
+        integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+        crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
     <script type="text/javascript" src="/app/app.js"></script>
   </head>
   <body ng-controller="HourglassController">
     <div id="main-wrapper">
-      <div class="hourglass">
+      <div class="hourglass" data-house="slytherin">
         <div class="house-name">Serpentard</div>
-        <div class="house-crest"><img src="/img/shield_sly.jpg" alt=""></div>
+        <div class="hourglass-body">
+          <div class="top"></div>
+          <div class="middle">
+            <div class="sand"></div>
+          </div>
+          <div class="bottom sand"></div>
+        </div>
+        <div class="house-crest"><img src="/img/shield_sly.png" alt=""></div>
         <div class="score">{{houses.slytherin.score}} points</div>
       </div>
-      <div class="hourglass">
+      <div class="hourglass" data-house="ravenclaw">
         <div class="house-name">Serdaigle</div>
-        <div class="house-crest"><img src="/img/shield_rav.jpg" alt=""></div>
+        <div class="hourglass-body">
+          <div class="top"></div>
+          <div class="middle">
+            <div class="sand"></div>
+          </div>
+          <div class="bottom sand"></div>
+        </div>
+        <div class="house-crest"><img src="/img/shield_rav.png" alt=""></div>
         <div class="score">{{houses.ravenclaw.score}} points</div>
       </div>
-      <div class="hourglass">
+      <div class="hourglass" data-house="gryffindor">
         <div class="house-name">Gryffondor</div>
-        <div class="house-crest"><img src="/img/shield_gry.jpg" alt=""></div>
+        <div class="hourglass-body">
+          <div class="top"></div>
+          <div class="middle">
+            <div class="sand"></div>
+          </div>
+          <div class="bottom sand"></div>
+        </div>
+        <div class="house-crest"><img src="/img/shield_gry.png" alt=""></div>
         <div class="score">{{houses.gryffindor.score}} points</div>
       </div>
-      <div class="hourglass">
+      <div class="hourglass" data-house="hufflepuff">
         <div class="house-name">Poufsouffle</div>
-        <div class="house-crest"><img src="/img/shield_huf.jpg" alt=""></div>
+        <div class="hourglass-body">
+          <div class="top"></div>
+          <div class="middle">
+            <div class="sand"></div>
+          </div>
+          <div class="bottom sand"></div>
+        </div>
+        <div class="house-crest"><img src="/img/shield_huf.png" alt=""></div>
         <div class="score">{{houses.hufflepuff.score}} points</div>
       </div>
     </div>
