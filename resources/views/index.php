5a08ffc561e77142ac40d4939b717195a72a5bdf
laloge_h <hugo.laloge@epitech.eu>
2016-09-09 17:17:25 +0200

    feat: Add index view

---
+++ resources/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Epi Hogwarts</title>
+  </head>
+  <body>
+    <div>Gryffondor : 0 points</div>
+    <div>Poufsouffle : 0 points</div>
+    <div>Serdaigle : 0 points</div>
+    <div>Serpentard : 0 points</div>
+  </body>
+</html>
