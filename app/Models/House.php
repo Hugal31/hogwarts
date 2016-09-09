f21d8f2c02a21e0afba0cade5e62dc565c49dd37
laloge_h <hugo.laloge@epitech.eu>
2016-09-09 19:28:26 +0200

    feat: Add House model

---
+++ app/Models/House.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laloge_h
+ * Date: 09/09/16
+ * Time: 19:00
+ */
+
+namespace App\Models;
+
+
+class House
+{
+
+}
---
+++ database/seeds/HouseSeeder.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laloge_h
+ * Date: 09/09/16
+ * Time: 18:52
+ */
