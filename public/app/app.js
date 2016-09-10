3551253aae45e16a32c1e23318a44fbef1acb5e6
laloge_h <hugo.laloge@epitech.eu>
2016-09-10 11:13:08 +0200

    feat: Add maximum score calculation for front
    - The maximum score on an hourglass is the maximum score ceil to the 200 multiple above

--- public/app/app.js
+++ public/app/app.js
@@ -3,11 +3,24 @@ var hogwartsApp = angular.module('hogwartsApp', []);
 hogwartsApp.controller('HourglassController', function HourglassController($scope, $http, $interval) {
 
     $scope.interval = 30000;
-    $scope.maxScore = 500;
+    $scope.maxScore = 200;
     $scope.houses = [];
 
     $scope.update = function () {
+
+        var calcMaxScore = function (houses) {
+            var max = 0;
+            houses.forEach(function (house) {
+                max = Math.max(max, house.score);
+            });
+            var maxScore = 200;
+            while (maxScore < max)
+                maxScore += 200;
+            return maxScore;
+        };
+
         $http.get('/api/v0/houses').then(function(response) {
+            $scope.maxScore = calcMaxScore(response.data);
             response.data.forEach(function (house) {
                 $scope.houses[house.name] = house;
                 var maxHeight = parseInt($('.hourglass[data-house="' + house.name + '"] .hourglass-body .middle').css('height'));
